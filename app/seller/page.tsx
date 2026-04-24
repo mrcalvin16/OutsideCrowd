e3ec724a6a27993ca906bfd1707a823db54adc2d
mrcalvin16 <kendrickcalvin21@gmail.com>
2026-04-24 06:30:55 -0500

    fix async clerk auth seller page

--- app/seller/page.tsx
+++ app/seller/page.tsx
@@ -2,8 +2,8 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import SellerDashboard from "@/components/SellerDashboard";
 
-export default function SellerPage() {
-  const { userId } = auth();
+export default async function SellerPage() {
+  const { userId } = await auth();
 
   if (!userId) {
     redirect("/sign-in");
